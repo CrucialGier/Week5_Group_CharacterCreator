69623b8ad8999be42568f4b12a994ab7045d0789
Bhawani Parajuli <parajulibhawani@gmail.com>
2016-07-28 09:46:04 -0700

    add new view page to display the created character

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -20,10 +20,19 @@ namespace CharacterCreator
       };
       Post["/character/create"]= _ =>{
         Character newCharacter = new Character(Request.Form["name"],Request.Form["klass"],Request.Form["bodyType"],Request.Form["weapon"],Request.Form["armor"],Request.Form["special"]);
+        newCharacter.Save();
         Dictionary<string, object> model = new Dictionary<string, object>();
-        model.Add("class", Class.GetAll());
-        model.Add("item", Item.GetAll());
-        model.Add("itemTypes", ItemType.GetAll());
+        Class characterClassName = Class.Find(newCharacter.GetClassId());
+        Item characterBodyType = Item.Find(newCharacter.GetBodyTypeId());
+        Item characterWeapon = Item.Find(newCharacter.GetWeaponId());
+        Item characterArmor = Item.Find(newCharacter.GetArmorId());
+        Item characterSpecial = Item.Find(newCharacter.GetSpecialId());
+        model.Add("className", characterClassName);
+        model.Add("bodyType", characterBodyType);
+        model.Add("weapon", characterWeapon);
+        model.Add("armor", characterArmor);
+        model.Add("special", characterSpecial);
+        model.Add("newCharacter", newCharacter);
         return View["new_character.cshtml", model];
       };
     }
