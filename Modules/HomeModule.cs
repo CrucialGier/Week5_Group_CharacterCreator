fa8b185d4db2e520e1a3d9770c64874f1fd4d910
Stewart Cole <stewart@>
2016-07-28 14:54:40 -0700

    Add crud to characters

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -41,14 +41,14 @@ namespace CharacterCreator
       };
       Get["/character/update/{id}"] =parameters=> {
         Dictionary<string, object> model = new Dictionary<string, object>();
-        model.Add("class", Class.GetAll());
+        model.Add("klass", Class.GetAll());
         model.Add("item", Item.GetAll());
         model.Add("itemTypes", ItemType.GetAll());
         Character currentCharacter = Character.Find(parameters.id);
         model.Add("current", currentCharacter);
         return View["update_character.cshtml", model];
       };
-      Post["/character/update/confirm/{id}"] =parameters=> {
+      Post["/character/confirm-update/{id}"] =parameters=> {
         Character currentCharacter = Character.Find(parameters.id);
         currentCharacter.SetBodyType(Request.Form["bodyType"]);
         currentCharacter.SetClass(Request.Form["klass"]);
@@ -57,6 +57,27 @@ namespace CharacterCreator
         currentCharacter.SetArmor(Request.Form["armor"]);
         currentCharacter.SetSpecial(Request.Form["special"]);
         currentCharacter.Update();
+        List<Character> AllCharacters = Character.GetAll();
+        return View["load_character.cshtml", AllCharacters];
+      };
+      Get["/character/delete/{id}"] =parameters=> {
+        Character currentCharacter = Character.Find(parameters.id);
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        Class characterClassName = Class.Find(currentCharacter.GetClassId());
+        Item characterBodyType = Item.Find(currentCharacter.GetBodyTypeId());
+        Item characterWeapon = Item.Find(currentCharacter.GetWeaponId());
+        Item characterArmor = Item.Find(currentCharacter.GetArmorId());
+        Item characterSpecial = Item.Find(currentCharacter.GetSpecialId());
+        model.Add("current", currentCharacter);
+        model.Add("className", characterClassName);
+        model.Add("bodyType", characterBodyType);
+        model.Add("weapon", characterWeapon);
+        model.Add("armor", characterArmor);
+        model.Add("special", characterSpecial);
+        return View["delete_character.cshtml", model];
+      };
+      Get["/character/delete-confirm/{id}"] =parameters=> {
+        Character.Delete(parameters.id);
         List<Character> AllCharacters = Character.GetAll();
         return View["load_character.cshtml", AllCharacters];
       };
